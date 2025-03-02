3e22f4810c2d672dc268bfb3aa6e826849a37281
pace11 <ryanjoker87@gmail.com>
2025-03-03 03:14:15 +0700

    feat: optimizing next.js

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,3 +1,22 @@
+// import Image from 'next/image'
+// import ImgHigh from '@/assets/img-high.jpg'
+// import HeavyComponent from '@/components/HeavyComponent'
+import dynamic from 'next/dynamic'
+import { useState } from 'react'
+
+const HeavyComponent = dynamic(() => import('@/components/HeavyComponent'), {
+  ssr: false,
+  loading: () => <p>Loading ...</p>,
+})
+
 export default function About() {
-  return <div>About Page</div>
+  const [show, setShow] = useState(false)
+  return (
+    <div>
+      <h1>About Page</h1>
+      <button onClick={() => setShow(true)}>Show Component</button>
+      {/* <img {...ImgHigh} /> */}
+      {show && <HeavyComponent />}
+    </div>
+  )
 }
